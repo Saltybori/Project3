ddb9ba9e2dbe6d1684ee4539f22911518e06e743
ian_c <ian_c@Thetool>
2023-04-05 22:41:26 -0400

    Innitial Commit

---
+++ src/demo3/DemoClass3.java
@@ -0,0 +1,15 @@
+package demo3;
+
+public class DemoClass3 {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+		
+		
+		System.out.println("Hello World");
+		
+		System.out.println("GitHub Lecture");
+	}
+	
+	
+}
